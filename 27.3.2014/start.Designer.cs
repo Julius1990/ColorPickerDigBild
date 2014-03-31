e0a9388955ac423a9969aefc99f4d0dd1274a825
Julius1990 <julius_weyl@gmx.net>
2014-03-31 11:04:58 +0200

    2 neue pictureboxes die farbverläaufe anzeigen

--- 27.3.2014/start.Designer.cs
+++ 27.3.2014/start.Designer.cs
@@ -28,117 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.TxtBrutto = new System.Windows.Forms.TextBox();
-            this.LblBrutto = new System.Windows.Forms.Label();
-            this.LblSteuer = new System.Windows.Forms.Label();
-            this.TxtSteuer = new System.Windows.Forms.TextBox();
-            this.LblNetto = new System.Windows.Forms.Label();
-            this.TxtNetto = new System.Windows.Forms.TextBox();
-            this.close = new System.Windows.Forms.Button();
-            this.Calc = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Blau = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // TxtBrutto
-            // 
-            this.TxtBrutto.Location = new System.Drawing.Point(107, 29);
-            this.TxtBrutto.Name = "TxtBrutto";
-            this.TxtBrutto.Size = new System.Drawing.Size(100, 20);
-            this.TxtBrutto.TabIndex = 1;
-            this.TxtBrutto.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // LblBrutto
-            // 
-            this.LblBrutto.AutoSize = true;
-            this.LblBrutto.Location = new System.Drawing.Point(22, 36);
-            this.LblBrutto.Name = "LblBrutto";
-            this.LblBrutto.Size = new System.Drawing.Size(35, 13);
-            this.LblBrutto.TabIndex = 2;
-            this.LblBrutto.Text = "Brutto";
-            this.LblBrutto.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // LblSteuer
-            // 
-            this.LblSteuer.AutoSize = true;
-            this.LblSteuer.Location = new System.Drawing.Point(22, 84);
-            this.LblSteuer.Name = "LblSteuer";
-            this.LblSteuer.Size = new System.Drawing.Size(38, 13);
-            this.LblSteuer.TabIndex = 4;
-            this.LblSteuer.Text = "Steuer";
-            // 
-            // TxtSteuer
-            // 
-            this.TxtSteuer.Location = new System.Drawing.Point(107, 77);
-            this.TxtSteuer.Name = "TxtSteuer";
-            this.TxtSteuer.Size = new System.Drawing.Size(100, 20);
-            this.TxtSteuer.TabIndex = 3;
-            // 
-            // LblNetto
-            // 
-            this.LblNetto.AutoSize = true;
-            this.LblNetto.Location = new System.Drawing.Point(22, 133);
-            this.LblNetto.Name = "LblNetto";
-            this.LblNetto.Size = new System.Drawing.Size(33, 13);
-            this.LblNetto.TabIndex = 6;
-            this.LblNetto.Text = "Netto";
-            this.LblNetto.Click += new System.EventHandler(this.label3_Click);
-            // 
-            // TxtNetto
-            // 
-            this.TxtNetto.Location = new System.Drawing.Point(107, 126);
-            this.TxtNetto.Name = "TxtNetto";
-            this.TxtNetto.ReadOnly = true;
-            this.TxtNetto.Size = new System.Drawing.Size(100, 20);
-            this.TxtNetto.TabIndex = 8;
-            // 
-            // close
-            // 
-            this.close.Location = new System.Drawing.Point(132, 176);
-            this.close.Name = "close";
-            this.close.Size = new System.Drawing.Size(75, 23);
-            this.close.TabIndex = 6;
-            this.close.Text = "Schließen";
-            this.close.UseVisualStyleBackColor = true;
-            this.close.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Calc
-            // 
-            this.Calc.Location = new System.Drawing.Point(25, 176);
-            this.Calc.Name = "Calc";
-            this.Calc.Size = new System.Drawing.Size(75, 23);
-            this.Calc.TabIndex = 5;
-            this.Calc.Text = "Berechnen";
-            this.Calc.UseVisualStyleBackColor = true;
-            this.Calc.Click += new System.EventHandler(this.Calc_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(246, 29);
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(128)))));
+            this.pictureBox1.Location = new System.Drawing.Point(338, 133);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(414, 271);
+            this.pictureBox1.Size = new System.Drawing.Size(257, 90);
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(128);
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(23, 133);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(256, 90);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Blau
+            // 
+            this.Blau.Location = new System.Drawing.Point(338, 89);
+            this.Blau.Name = "Blau";
+            this.Blau.Size = new System.Drawing.Size(257, 20);
+            this.Blau.TabIndex = 11;
+            this.Blau.TextChanged += new System.EventHandler(this.Blau_TextChanged);
             // 
             // start
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(672, 312);
+            this.ClientSize = new System.Drawing.Size(620, 247);
+            this.Controls.Add(this.Blau);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.Calc);
-            this.Controls.Add(this.close);
-            this.Controls.Add(this.LblNetto);
-            this.Controls.Add(this.TxtNetto);
-            this.Controls.Add(this.LblSteuer);
-            this.Controls.Add(this.TxtSteuer);
-            this.Controls.Add(this.LblBrutto);
-            this.Controls.Add(this.TxtBrutto);
             this.Name = "start";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.start_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -146,15 +82,9 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox TxtBrutto;
-        private System.Windows.Forms.Label LblBrutto;
-        private System.Windows.Forms.Label LblSteuer;
-        private System.Windows.Forms.TextBox TxtSteuer;
-        private System.Windows.Forms.Label LblNetto;
-        private System.Windows.Forms.TextBox TxtNetto;
-        private System.Windows.Forms.Button close;
-        private System.Windows.Forms.Button Calc;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox Blau;
 
     }
 }
--- 27.3.2014/start.cs
+++ 27.3.2014/start.cs
@@ -16,57 +16,64 @@ namespace _27._3._2014
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if(MessageBox.Show("Wirklich beenden ?", Application.ProductName, MessageBoxButtons.YesNo, MessageBoxIcon.Question)==DialogResult.Yes)
-                this.Close();
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Calc_Click(object sender, EventArgs e)
-        {
-            double brutto, steuer, netto;
-            if (double.TryParse(this.TxtBrutto.Text, out brutto))
-            {
-                if (double.TryParse(this.TxtSteuer.Text, out steuer))
-                {
-                    netto = brutto * 100 / (100 + steuer);
-                    this.TxtNetto.Text = netto.ToString();
-                }
-                else
-                {
-                    // Die Steuer-Eingabe ist nicht OK 
-                    this.TxtBrutto.Text = null;
-                    MessageBox.Show("Der Steuerwert ist ungültig");
-                }
-            }
-            else
-            {
-                // Die Brutto-Eingabe ist nicht OK 
-                this.TxtBrutto.Text = null;
-                MessageBox.Show("Der Bruttobetrag ist ungültig");
-            }
-
-        }
-
+        
         private void pictureBox1_Click(object sender, EventArgs e)
         {
+            this.Blau.Text = this.PointToClient(Cursor.Position).ToString();
+            /*int xPos = pictureBox1.Location.ToString;
+
+            int yPos = MousePosition.Y - pictureBox1.Location.Y;
+
+            string coor = xPos.ToString();
+            coor += "/";
+            coor += yPos.ToString();*/
+            this.Blau.Text = pictureBox1.
+        }
+
+
+        private void start_Load(object sender, EventArgs e)
+        {
+            Bitmap schwarzRot = new Bitmap(256, 90);
+            for(int y=0;y<90;y++){
+                for(int x=0;x<256;x++){
+                    schwarzRot.SetPixel(x,y,Color.FromArgb(x,0,0));
+                }
+            }
+            pictureBox1.Image=schwarzRot;
+
+            Bitmap gruenBlau = new Bitmap(1024, 90);
+            for (int a = 0; a < 90; a++)
+            {
+                for (int eins = 0; eins <=255; eins++)
+                {
+                    gruenBlau.SetPixel(eins,a,Color.FromArgb(255,eins,0));
+                }
+                for (int zwei = 0; zwei <= 255; zwei++)
+                {
+                    gruenBlau.SetPixel(zwei+255,a,Color.FromArgb(255-zwei,255,0));
+                }
+                for (int drei = 0; drei <= 255; drei++)
+                {
+                    gruenBlau.SetPixel(drei + 511, a, Color.FromArgb(0, 255, drei));
+                }
+                for (int vier = 0; vier <= 255; vier++)
+                {
+                    gruenBlau.SetPixel(vier + 767, a, Color.FromArgb(0, 255 - vier, 255));
+                }
+            }
+            pictureBox2.Image=gruenBlau;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Size = new System.Drawing.Size(255, 90);
             
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Blau_TextChanged(object sender, EventArgs e)
+        {
             
         }
 
